f2360a1d22eca3beafd656becbc18e2f272a0b1a
wangjiezhe <wangjiezhe@gmail.com>
2024-12-16 21:05:05 +0800

    Run `git lfs install` in the repository

--- entrypoint.sh
+++ entrypoint.sh
@@ -23,9 +23,6 @@ EOF
 git config --global user.name "$INPUT_USER_NAME"
 git config --global user.email "$INPUT_USER_EMAIL"
 
-# configure LFS
-git lfs install
-
 # install hexo env
 npm install hexo-deployer-git --save
 
@@ -39,14 +36,24 @@ fi
 # deployment
 if [ "$INPUT_COMMIT_MSG" = "none" ]
 then
+    pushd .deploy_git
+    git init
+    git lfs install
+    popd
     hexo g -d
 elif [ "$INPUT_COMMIT_MSG" = "" ] || [ "$INPUT_COMMIT_MSG" = "default" ]
 then
     # pull original publish repo
     NODE_PATH=$NODE_PATH:$(pwd)/node_modules node /sync_deploy_history.js
+    pushd .deploy_git
+    git lfs install
+    popd
     hexo g -d
 else
     NODE_PATH=$NODE_PATH:$(pwd)/node_modules node /sync_deploy_history.js
+    pushd .deploy_git
+    git lfs install
+    popd
     hexo g -d -m "$INPUT_COMMIT_MSG"
 fi
 
